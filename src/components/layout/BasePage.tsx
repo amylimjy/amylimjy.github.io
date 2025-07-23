ac54d09f81191be48597d47d9f6ef636fa45eafd
marcusleowhm <leowmarcus13@hotmail.com>
2025-07-24 01:35:55 +0800

    [TECH][marcus] refactor: change grid starting column for page

--- src/components/layout/BasePage.tsx
+++ src/components/layout/BasePage.tsx
@@ -9,7 +9,7 @@ export const BasePage = ({
       id="page__content"
       className={"flex-1 pt-14"}
     >
-      <section className="md:col-start-2 col-span-10 p-4 shadow-xl rounded-sm">{children}</section>
+      <section className="md:col-start-3 col-span-8 p-4 shadow-xl rounded-sm">{children}</section>
     </GridLayout>
   );
 };
