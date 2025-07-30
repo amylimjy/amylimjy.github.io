d472625e4ac0ccaf6b5ef7555f45afc0fdc6d275
Marcus LEOW <leowmarcus13@hotmail.com>
2025-07-31 00:57:41 +0800

    [TECH][marcus] feat: change conference section to a single div

--- src/components/page-sections/ResumeConference.tsx
+++ src/components/page-sections/ResumeConference.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { ResumeSectionTitle } from "./ResumeSectionTitle";
 import jsonData from "@/../content/conference.json";
+import { constructAuthorsString } from "@/utils/StringUtils";
+
+interface ConferenceEntry {
+  speakers: string[];
+  title: string;
+  organization: string;
+  location?: string;
+  date: string;
+}
 
 export const ResumeConference = (): React.JSX.Element => {
+
+  const conferences: ConferenceEntry[] = jsonData;
+
   return (
     <div className="flex-1">
       <ResumeSectionTitle title="CONFERENCE" />
-      {jsonData.map((item, index) => (
-        <div key={index} className="grid grid-cols-4 my-2">
-          <div className={`col-span-3 col-start-1 text-left ml-4`}>
-            {item.speakers}, {item.title}
-          </div>
-          <div className={`col-start-4 text-right mr-4 text-xs`}>
-            {item.year}
+      {conferences.map((item, index) => (
+        <div key={index} className="my-4">
+          <div className="mx-4">
+            {item.speakers.map((author, index) => constructAuthorsString(author, item.speakers, index))} {item.title} {item.date && `(${item.date}).`} {`${item.organization} ${item.location}`}
           </div>
         </div>
       ))}
