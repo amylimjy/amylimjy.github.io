8deaf5b4575fa84fd55eafc9cf6ab49a8418ad47
Marcus LEOW <leowmarcus13@hotmail.com>
2026-06-12 23:47:29 +0800

    [TECH][marcus] feat: add some comments for the author name formatting logic

--- src/utils/StringUtils.tsx
+++ src/utils/StringUtils.tsx
@@ -18,8 +18,22 @@ const isSecondLastItem = (array: string[], index: number) => {
 
 export const constructAuthorsString = (author: string, array: string[], index: number) => {
   return (<>
+    {/* 
+    The logic here is if authorName name is the last item in the list, 
+    AND
+    it is not the only one in the list, we add an & before the name 
+     */}
     {isLastItem(array, index) && !isTheOnlyItem(array) && <span> & </span>}
+
+    {/* if the author name matches authorName, we bold it */}
     {author.includes(authorName) ? <span className="font-bold">{author}</span> : <span>{author}</span>}
+
+    {/* 
+    The logic here is if there are more authors while iterating
+    AND
+    the name is not the second last author, we added a comma.
+    (if there are more authors and the name IS the second last, we don't add commas)
+     */}
     {hasMoreAuthor(array, index) && !isSecondLastItem(array, index) && <span>, </span>}
   </>)
 }
